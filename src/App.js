c35362e3655b05d9f4cd41b61684f8a0a136564c
Ole Anders Smith <ole.anders.smith@lyse.net>
2018-11-15 18:33:35 +0100

    tekstendring

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    Edit <code>src/App.js</code> and save to reload Dette er Kantega workshop. Endring 3.	
+		    Edit <code>src/App.js</code> and save to reload Dette er Kantega workshop. Endring 4.	
           </p>
           <a
             className="App-link"
